c3083db738a13199253a676aed73efcc8ec78259
Michelle D Zhang <zhang.michelle.d@gmail.com>
2017-05-17 08:09:20 -0400

    Hamming distance

---
+++ algorithms/README.md
@@ -0,0 +1,18 @@
+> Write a function that computes the hamming distance between two iterables. The hamming distance is defined as the number of positions where the symbols are different. <https://en.wikipedia.org/wiki/Hamming_distance>
+
+##### Solution
+
+[See solution](./hamming_distance.py)
+
+> Uniquely Encode a string to as short a length as possible then decode it. Feel free to look up established algorithms to attempt the shortest length possible, but cite the algorithm you have chosen. We will compare your compression against Run Length Encoding, listed next to each example is the length of the compressed string using Run Length Encoding.
+
+##### Solution
+
+[See solution](./string_compression.py)
+
+> Given a tree, find the postorder traversal: Leftmost leaf, Leaf to the right of that leaf, root of those, left of the next right branch, etc.
+<https://en.wikipedia.org/wiki/Tree_traversal#Post-order>
+
+##### Solution
+
+[See solution](./post_order_traversal.py)
--- algorithms/hamming_distance.py
+++ algorithms/hamming_distance.py
@@ -1,11 +1,41 @@
-def hamming_distance(vector1: iterable, vector2: iterable) -> int:
-    # TODO
-    pass
+from typing import Iterable
+import unittest
 
-assert hamming_distance([],[]) == 0
-assert hamming_distance([0,1],[0,1]) == 0
-assert hamming_distance("00","01") == 1
-assert hamming_distance("karolin", "kathrin") == 3
-assert hamming_distance("karolin", "kerstin") == 3
-assert hamming_distance((1,0,1,1,1,0,1), (1,0,0,1,0,0,1)) == 2
-assert hamming_distance("2173896", "2233796") == 3
+
+def hamming_distance(vector1: Iterable, vector2: Iterable) -> int:
+    distance = 0
+
+    for e1, e2 in zip(vector1, vector2):
+        if e1 != e2:
+            distance += 1
+
+    if len(vector2) > len(vector1):
+        distance += len(vector2) - len(vector1)
+
+    if len(vector1) > len(vector2):
+        distance += len(vector1) - len(vector2)
+
+    return distance
+
+
+class TestHammingDistance(unittest.TestCase):
+    def test_given(self):
+        assert hamming_distance([], []) == 0
+        assert hamming_distance([0, 1], [0, 1]) == 0
+        assert hamming_distance("00", "01") == 1
+        assert hamming_distance("karolin", "kathrin") == 3
+        assert hamming_distance("karolin", "kerstin") == 3
+        assert hamming_distance((1, 0, 1, 1, 1, 0, 1), (1, 0, 0, 1, 0, 0, 1)) == 2
+        assert hamming_distance("2173896", "2233796") == 3
+
+    def test_unequal_len(self):
+        assert hamming_distance([0, 1], [0, 1, 2]) == 1
+        assert hamming_distance([0, 1, 2], [0, 1]) == 1
+        assert hamming_distance("kathy", "kathryn") == 3
+        assert hamming_distance("kathryn", "kathy") == 3
+        assert hamming_distance((), (1, 2, 3, 4)) == 4
+        assert hamming_distance((1, 2, 3, 4), ()) == 4
+
+
+if __name__ == '__main__':
+    unittest.main()
