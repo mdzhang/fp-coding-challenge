1c872b188f766661f1e78f71ccc5dade15389a52
Michelle D Zhang <zhang.michelle.d@gmail.com>
2017-05-17 09:03:14 -0400

    algos - post order traversal

--- algorithms/post_order_traversal.py
+++ algorithms/post_order_traversal.py
@@ -1,13 +1,47 @@
+import unittest
+
+
 def post_order_traversal(tree):
-    # TODO
-    pass
+    traversal = []
+
+    for child in tree[1:]:
+        child_traversal = post_order_traversal(child)
+        traversal.extend(child_traversal)
+
+    traversal.append(tree[0])
+
+    return traversal
 
 
-Tree1 = [1, [2, [4], [5], [3]]]
-Tree2 = ['f', ['b', ['a'], ['d', ['c'], ['e']]], ['g', ['i', ['h']]]]
-Tree3 = ['re', ['b', ['orn'], ['ate']], ['alize', ['s']], ['lief'], ['d', ['der']]]
+class TestPostOrderTraversal(unittest.TestCase):
+    def test_given(self):
+        tree1 = [1, [2, [4], [5], [3]]]
+        expected1 = [4, 5, 3, 2, 1]
 
-assert post_order_traversal(Tree2) == [4,5,2,3,1]
-assert post_order_traversal(Tree2) == ['a', 'c', 'e', 'd', 'b', 'h', 'i', 'g', 'f']
-assert post_order_traversal(Tree3) == ['orn', 'ate', 'b', 's', 'alize', 'lief', 'der', 'd',
-'re']
+        self.assertEqual(post_order_traversal(tree1), expected1)
+
+        tree2 = ['f', ['b', ['a'], ['d', ['c'], ['e']]], ['g', ['i', ['h']]]]
+        expected2 = ['a', 'c', 'e', 'd', 'b', 'h', 'i', 'g', 'f']
+
+        self.assertEqual(post_order_traversal(tree2), expected2)
+
+        tree3 = [
+            're', ['b', ['orn'], ['ate']],
+            ['alize', ['s']], ['lief'], ['d', ['der']]
+        ]
+        expected3 = [
+            'orn', 'ate', 'b', 's', 'alize',
+            'lief', 'der', 'd', 're'
+        ]
+
+        self.assertEqual(post_order_traversal(tree3), expected3)
+
+    def test_empty(self):
+        self.assertEqual(post_order_traversal([]), [])
+
+    def test_single(self):
+        self.assertEqual(post_order_traversal([1]), [1])
+
+
+if __name__ == '__main__':
+    unittest.main()
